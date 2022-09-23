2b428ecff1b26e5bb043269146c1f2d8b1b3396a
hasija <shasija95@gmail.com>
2022-09-23 10:50:41 +0530

    Time: 6156 ms (5.51%), Space: 13.8 MB (97.64%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
@@ -1,14 +1,14 @@
 class Solution:
+    def count_bits(self, num):
+        count = 0
+        while num:
+            num>>=1
+            count+=1
+        return count
     def concatenatedBinary(self, n: int) -> int:
-        binary = format(n, "b")
         out = 0
-        new_s = ""
-        for ind in range(1,n+1):
-            new_s += format(ind, "b")
-        # print (new_s)
-        # for ind in range(len(new_s)-1, -1, -1):
-        #     out+=(2**ind)*int(new_s[ind])
-        out = int(new_s, 2)
-        return out%(10**9+7)
-            
+        mod = 10**9 + 7
+        for val in range(n+1):
+            out = ((out<<self.count_bits(val))+val)%mod
+        return out
         
