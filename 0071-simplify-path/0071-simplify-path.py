4dd44c10c0c09518d270c658f5a8e61596637850
hasija <shasija95@gmail.com>
2023-04-13 01:11:32 +0530

    Time: 28 ms (92.10%), Space: 13.9 MB (27.95%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,53 +1,19 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        final_arr = []
-        # to store value
-        tmp_string = ""
-        path += '/'
-        n = len(path)
-        # print (path)
-        prev_slash = 0
-        for i in range(n):
-            curr_val = path[i]
-            # print (i, curr_val)
-            # condition one, to get values between two slashes
-            if curr_val =='/':
-                # print(prev_slash, 'prev slash')
-                # print(i , 'curr_slash')
-                # print (tmp_string, path[prev_slash+1:i], 'new entries between slashes')
-                # print (final_arr)
-                # condition 2nd
-                if tmp_string =='..':
-                    if len(final_arr)>0:
-                        final_arr.pop()
-                # Condition 3rd single dot
-                elif tmp_string == '.':
-                    pass
-                else:
-                    if len(tmp_string)>0:
-                        final_arr.append(tmp_string)
-                
-                tmp_string = ""
-                # operations are done
-                prev_slash = i
-            else:
-                tmp_string+=curr_val
-        
-        final_str = "/"
-        
-        arr_len = len(final_arr)
-        # arr_len: 2
-        # ind 0 , 1 --> final_ind 1 ==2-1
-        
-        for ind in range(arr_len):
-            # last value
-            val = final_arr[ind]
-            if ind == arr_len-1:
-                final_str+=val
-            else:
-                final_str+=val+'/'
-        return final_str
-                
+        path +="/"
+        past = 0
+        arr = []
+        for ind,val in enumerate(path):
+            if val=='/':
+                tmp = path[past+1:ind]
+                if arr and tmp=='..':
+                    arr.pop()
+                elif tmp and tmp!='.' and tmp!='..':
+                    arr.append(tmp)
+                past = ind                
+        return '/'+'/'.join(arr)
+                    
+            
         
         
             
