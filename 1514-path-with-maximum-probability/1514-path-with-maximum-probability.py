c9870c8fd973edab6e2ad313966168d9130bb316
hasija <shasija95@gmail.com>
2024-08-31 14:03:47 +0530

    Time: 568 ms (35.47%), Space: 28.9 MB (41.45%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -1,23 +1,27 @@
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
-        graph = defaultdict(list)
-        for idx, vals in enumerate(edges):
-            u,v = vals
-            graph[u].append((v, succProb[idx]))
-            graph[v].append((u, succProb[idx]))
-        
-        visited = set()
-        min_dist = defaultdict(int)
-        arr = [(-1,start_node)]
-        while arr:
-            prob, node = heapq.heappop(arr)
-            if node in visited:
-                continue
-            visited.add(node)
-            for nxt_node, nxt_prob in graph[node]:
-                if -1*prob*nxt_prob>min_dist[nxt_node]:
-                    min_dist[nxt_node]=-1*prob*nxt_prob
-                    heapq.heappush(arr, (-min_dist[nxt_node],nxt_node))
-        # print (min_dist)
-        return min_dist[end_node]
+
+        graph = defaultdict(list)    
+        for idx, edge in enumerate(edges):
+            u,v = edge
+            prob = succProb[idx] 
+            graph[u].append((v,prob))
+            graph[v].append((u,prob))
             
+        def shortest_path(start):
+            max_prob = [0]*n
+            max_prob[start]=1
+            heap = [(-1, start)]
+            visited = set()
+            while heap:
+                prob, node = heapq.heappop(heap)
+                visited.add(node)
+                for v,nxt_prob in graph[node]:
+                    if v not in visited and max_prob[v]<-prob*nxt_prob:
+                        max_prob[v] = -prob*nxt_prob
+                        heapq.heappush(heap, (prob*nxt_prob, v))
+            print(max_prob)
+            return max_prob[end_node]
+        return shortest_path(start_node)
+                        
+                    
