9758d33b53d337a949af7a6476525f49b1035083
SHUBHAM HASIJA <shasija95@gmail.com>
2024-11-09 19:20:27 +0530

    Time: 0 ms (100.00%), Space: 16.7 MB (33.33%) - LeetHub

--- 3133-minimum-array-end/3133-minimum-array-end.py
+++ 3133-minimum-array-end/3133-minimum-array-end.py
@@ -1,24 +1,37 @@
 class Solution:
     def minEnd(self, n: int, x: int) -> int:
-        x_bits = bin(x)[2:]
-        if n==1:
-            return x
-        # number to find
-        new_n = n-1
-        new_n = bin(new_n)[2:]
-        new_n = list(reversed(new_n))
-        cnt = 0
-        ans = ""
-        for i in reversed(x_bits):
-            if i=='0' and  cnt<len(new_n):
-                ans = new_n[cnt]+ans
-                cnt+=1
-            else:
-                ans = i+ans
-        # print ("def" ,ans)
-        # print ("rem", ''.join(new_n[cnt:]))
-        new_n = reversed(new_n[cnt:])
-        ans = ''.join(new_n)+ans
+        min_bit = list(bin(x)[2:][::-1])
+        LEN = len(min_bit)
+        idx_arr = []
+        last = x
         
-        # print(ans)
-        return int(ans,2)
+        def find_all_zero_bit():
+            cnt = 0
+            for idx, i in enumerate(min_bit):
+                if i == '0':
+                    cnt+=1
+                    idx_arr.append(idx)
+            return cnt
+        cnt = find_all_zero_bit()
+        n_bits = bin(n-1)[2:][::-1]
+        # print (n_bits)
+        LEN = len(idx_arr)
+        # print("len of idx arr", LEN, idx_arr)
+        for idx, i in enumerate(n_bits):
+            if i=='1':
+                if idx<LEN:
+                    # print ("here1")
+                    min_bit[idx_arr[idx]]='1'
+                else:
+                    # print ("here2")
+                    min_bit.append('1')
+            else:
+                if idx>=LEN:
+                    # print ("here3")
+                    min_bit.append('0')
+        # print (''.join(min_bit[::-1]))
+        return int(''.join(min_bit[::-1]),2)
+
+            
+            
+            
