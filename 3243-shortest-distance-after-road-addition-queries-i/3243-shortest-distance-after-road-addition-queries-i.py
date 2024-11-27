35c81ec073018d9d4c625a81e7bf2152264f46f6
SHUBHAM HASIJA <shasija95@gmail.com>
2024-11-27 10:03:05 +0530

    Time: 367 ms (85.72%), Space: 17.9 MB (8.31%) - LeetHub

--- 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.py
+++ 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.py
@@ -1,44 +1,40 @@
+
+
 class Solution:
     def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
-        roads = defaultdict(list)
-        curr_max = n-1
-        ans = []
-        for i in range(n-1):
-            roads[i].append(i+1)
+        hmap = defaultdict(set)
+        for i in range(n):
+            if i!=n-1:
+                hmap[i].add(i+1)
 
+
+        def add_path(u,v):
+            hmap[u].add(v)
+        
         def bfs():
             queue = deque([0])
             ans = 0
             visited = set()
             while True:
-                # print ("start with queue", queue)
-                new_queue = deque([])
+                new_queue = deque()
                 while queue:
-                   
-                    
-                    idx = queue.popleft()
-                    # print ("working on idx", idx)
-                    if idx in visited:
-                        # print ("already visited")
-                        continue
-                    visited.add(idx)
-                    if idx == n-1:
-                        # print ("match found", ans)
-                        return ans
-                    for new in roads[idx]:
-                        # print ("new roads from ",idx, "to ", new)
-                        new_queue.append(new)
+                    node = queue.popleft()
+                    for new_node in hmap[node]:
+                        if new_node in visited:
+                            continue
+                        new_queue.append(new_node)
+                        visited.add(new_node)
+                        if new_node==n-1:
+                            return ans+1
                 ans+=1
-                queue = new_queue.copy()
-                # print ("new_queue", new_queue)
-                if len(new_queue)==0:
-                    return -1
+                if new_queue:
+                    queue = new_queue
+                else:
+                    return n-1
             
-
-
-        for i,j in queries:
-            # print ("adding new road", i, j)
-            roads[i].append(j)
-            ans.append(bfs())
-        return ans
-        
+        out = []
+        for u,v in queries:
+            add_path(u,v)
+            out.append(bfs())
+        return out
+            
